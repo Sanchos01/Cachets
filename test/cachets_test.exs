ab7318e90bc17615a1bbd4b0946241b312c69b96
Alexander Tuktarov <sanchos.1991@mail.ru>
2017-06-08 21:26:30 +0300

    upgrade tests

--- test/cachets_test.exs
+++ test/cachets_test.exs
@@ -8,19 +8,26 @@ defmodule CachetsTest do
     assert [{:key, 123}] == Cachets.Exact.get(:key)
   end
 
+  test "process-deleter is exist" do
+    Cachets.Exact.add(:key, 123, ttl: 5)
+    assert length(:pg2.get_local_members(@pg2_group)) == 1
+  end
+
+  test "record and destr-process self-destroyed" do
+    Cachets.Exact.add(:key, 123, ttl: 5)
+    :timer.sleep(6) # Need one more millisecond for self-destroying process
+    assert Cachets.Exact.get(:key) == []
+    assert length(:pg2.get_local_members(@pg2_group)) == 0
+  end
+
   test "add to common" do
     Cachets.Common.add(:key, 123)
     assert [{:key, 123}] == Cachets.Common.get(:key)
   end
 
-  test "process-deleter is exist" do
-    Cachets.Exact.add(:key, 123, ttl: 7000)
-    assert length(:pg2.get_local_members(@pg2_group)) == 1
-  end
-
-  test "record self-destroyed" do
-    Cachets.Exact.add(:key, 123, ttl: 5)
-    :timer.sleep(5)
-    assert Cachets.Exact.get(:key) == []
+  test "delete from common" do
+    Cachets.Common.add(:key, 123)
+    Cachets.Common.delete(:key)
+    assert Cachets.Common.get(:key) == []
   end
 end
